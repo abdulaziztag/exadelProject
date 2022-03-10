431e2ef507493ba6166940a0baa65dd8a4c31230
Abdulaziz <abdulazizmannopov07@gmail.com>
2022-03-10 22:59:17 +0500

    Homework 2

--- main.js
+++ main.js
@@ -1,42 +1,60 @@
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
 const express = require('express')
+const {jwtCallback} = require('./passport');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
 require('dotenv').config()
 
+const { loginUser, users, registerUser } = require('./database')
 const app = express()
 
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(passport.initialize());
 
-const users = [
-  {
-    name: 'Abdulaziz',
-    age: 20,
-    email: 'qwerty123@gmail.com',
-    password: "123456"
-  },
-  {
-    name: 'John',
-    age: 12,
-    email: 'asdfg123@gmail.com',
-    password: "123456"
-  },
-  {
-    name: 'Alice',
-    age: 21,
-    email: 'alicelol@gmail.com',
-    password: "123456"
-  },
-  {
-    name: 'Melisa',
-    age: 17,
-    email: 'melisa123@gmail.com',
-    password: "123456"
-  },
-  {
-    name: 'Vale',
-    age: 27,
-    email: 'valevalir@gmail.com',
-    password: "123456"
+const opts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET,
+}
+
+passport.use(new JwtStrategy(opts, jwtCallback));
+
+const auth = passport.authenticate('jwt', { session: false });
+
+app.post('/register', (req, res) => {
+  const { email, password } = req.body;
+  registerUser({ email, password });
+
+  res.json(users);
+})
+
+app.post('/login', (req, res) => {
+  const user = loginUser(req.body.email, req.body.password)
+  if (user) {
+
+    const payload = {
+      id: user.id,
+      email: user.email
+    }
+
+    const token = jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_EXPIRES_IN }
+    );
+
+    res.status(200).json({
+      id: user.id,
+      email: user.email,
+      token: `Bearer ${token}`,
+    })
+  } else {
+    res.status(401).json({ message: 'Invalid credentials' });
   }
-]
+})
 
 app.get('/', (req, res) => {
   res.send(`
@@ -54,7 +72,7 @@ app.delete('/:id', (req, res) => {
   res.send(`Successfully deleted! New db: ${JSON.stringify(users)}`)
 })
 
-app.post('/', (req, res) => {
+app.post('/', auth, (req, res) => {
   let user = users.find((key) => {
     return key.email === req.body.email
   })
