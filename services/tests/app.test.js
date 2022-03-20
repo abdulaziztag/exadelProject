553afd2b09ea05f9f980e7890b8c89a82ad20c80
Abdulaziz <abdulazizmannopov07@gmail.com>
2022-03-20 23:22:23 +0500

    Homework 4 tasks successfully done and test are passed successfully

---
+++ services/tests/app.test.js
@@ -0,0 +1,30 @@
+const app = require('../../main')
+const supertest = require('supertest')
+const mongoose = require('mongoose')
+const mongoUrl = `${process.env.MONGODB_URI}${process.env.DB_NAME}`;
+
+describe('app', () => {
+
+  beforeAll(async () => {
+    await mongoose.disconnect();
+    await mongoose.connect(mongoUrl);
+  });
+
+  afterAll(async () => {
+    await mongoose.disconnect();
+  });
+
+  describe('POST /users', () => {
+
+    it('should write given data to db and return it!', async () => {
+      const response = await supertest(app)
+        .post('/users')
+        .send({ email: "xxxx@gmail.com", age: "44", name: "Gleb" });
+
+      expect(response.status).toBe(200);
+      expect(response.header['content-type']).toBe('application/json; charset=utf-8');
+    });
+
+  })
+
+});
