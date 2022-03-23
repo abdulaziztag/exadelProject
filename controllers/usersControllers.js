e0dac09c5d83e4b0d6674d14be9947b73a68fe4a
Abdulaziz <abdulazizmannopov07@gmail.com>
2022-03-23 18:33:09 +0500

    Fixed: moved functions of requests to controllers

---
+++ controllers/usersControllers.js
@@ -0,0 +1,40 @@
+const User = require('../models/users');
+
+const getUsers = async (req, res) => {
+  const users = await User.find({});
+  res.send(users)
+}
+
+const addUsers = async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+}
+
+const editUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(
+      req.body._id,
+      req.body,
+      {new: true}
+    );
+    res.status(204).json(user);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+}
+
+const deleteUser = async (req, res) => {
+  await User.deleteOne({_id: req.params.id})
+  res.send({message: 'Deleted successfully!'})
+}
+
+module.exports = {
+  getUsers,
+  addUsers,
+  editUser,
+  deleteUser
+}
--- services/tests/app.test.js
+++ services/tests/app.test.js
@@ -19,7 +19,7 @@ describe('app', () => {
     it('should write given data to db and return it!', async () => {
       const response = await supertest(app)
         .post('/users')
-        .send({ email: "xxxx@gmail.com", age: "44", name: "Gleb" });
+        .send({ email: "xxxx123@gmail.com", age: "44", name: "Gleb" });
 
       expect(response.status).toBe(200);
       expect(response.header['content-type']).toBe('application/json; charset=utf-8');
