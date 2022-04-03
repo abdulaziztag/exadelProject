ae7221c68bbac6455a0e36792fe0f09a70dd72dd
Abdulaziz <abdulazizmannopov07@gmail.com>
2022-04-03 23:57:01 +0500

    Added: Home page(/), Login page(/login), header

---
+++ client/src/app/pages/statistics/statistics.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core'
+import { StatisticsComponent } from './statistics.component'
+
+@NgModule({
+  declarations: [StatisticsComponent],
+  imports: [],
+  providers: []
+})
+export class StatisticsModule {}
